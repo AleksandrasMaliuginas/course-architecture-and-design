07379ef0f5661908a02584295d5a684a073bc6e1
Aleksandras <aleksandrasm348@gmail.com>
2021-09-26 13:13:15 +0300

    Initial Validation implementation added

---
+++ src/com/alemal/gintare/validation/PasswordChecker.java
@@ -0,0 +1,19 @@
+package com.alemal.gintare.validation;
+
+public class PasswordChecker {
+    public static boolean hasInput(String email) {
+        return false;
+    }
+
+    public static boolean isLengthValid(String s) {
+        return false;
+    }
+
+    public static boolean containsUppercaseLetters(String password) {
+        return false;
+    }
+
+    public static boolean containsSpecialCharacter(String s) {
+        return false;
+    }
+}
---
+++ src/com/alemal/gintare/validation/PhoneValidator.java
@@ -0,0 +1,19 @@
+package com.alemal.gintare.validation;
+
+public class PhoneValidator {
+    public static boolean hasInput(String email) {
+        return false;
+    }
+
+    public static boolean containsNumbersOnly(String s) {
+        return false;
+    }
+
+    public static String checkPrefix(String countryCode, String s) {
+        return null;
+    }
+
+    public static boolean isCorrectLength(String countryCode, String phoneNumber_correct) {
+        return false;
+    }
+}
